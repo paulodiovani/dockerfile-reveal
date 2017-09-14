e235c5944c420620dcde8f1104997d177fb3f41c
Paulo Diovani <paulo@diovani.com>
2017-09-14 20:21:39 -0300

    Add script to read image build steps

---
+++ index.js
@@ -0,0 +1,59 @@
+const { exec } = require('child_process')
+
+const compose = (...fns) => fns.reduce((f, g) => (...args) => f(g(...args)))
+
+const dockerInspect = (image) => {
+  return new Promise((resolve, reject) => {
+    exec(`docker inspect ${image}`, (err, stdout, stderr) => {
+      if (err) return reject(err)
+      if (stderr) return reject(stderr)
+      resolve(stdout)
+    })
+  })
+}
+
+const parseCmd = compose(
+  (cmd) => cmd.trim(),
+  (cmd) => cmd.replace(/\s+/g, ' '),
+  (cmd) => cmd[1] ? cmd[2] : `RUN ${cmd[2]}`,
+  (cmd) => /(#\(nop\))?(.+)?/.exec(cmd),
+  (cmd) => cmd.join(' '),
+  (cmd) => cmd.slice(2)
+)
+
+const parseInspect = (stdout) => {
+  const [{
+    RepoTags: [Tag],
+    Parent,
+    ContainerConfig: {
+      Cmd
+    }
+  }] = JSON.parse(stdout)
+
+  return { Line: [parseCmd(Cmd)], Parent, Tag }
+}
+
+const recurse = ({Line, Parent, Tag}) => {
+  if (Parent) {
+    return dockerInspect(Parent)
+      .then(parseInspect)
+      .then(recurse)
+      .then((previous) => Line.concat(previous))
+  }
+
+  return Line.concat([`FROM ${Tag}`])
+}
+
+const format = compose(
+  (steps) => steps.join('\n'),
+  (steps) => steps.reverse()
+)
+
+const revealDockerfile = (image) => {
+  return dockerInspect(image)
+    .then(parseInspect)
+    .then(recurse)
+    .then(format)
+}
+
+module.exports = revealDockerfile
